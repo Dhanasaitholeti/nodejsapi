a3dd6e5c73f0fa2dc8205dd636a54a0d441785ba
Dhanasaitholeti <86939592+Dhanasaitholeti@users.noreply.github.com>
2022-11-09 16:33:35 +0530

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const data = require("../data/kiet")
 
 router.get("/",(req,res) => {
-    res.set('Access-Control-Allow-Origin', '*')
+//     res.set('Access-Control-Allow-Origin', '*')
     res.json(data)
 });
 
 router.get('/student/:id',(req,res) => {
-    res.set('Access-Control-Allow-Origin', '*')
+//     res.set('Access-Control-Allow-Origin', '*')
     data[req.params.id]?res.json(data[req.params.id]):(res.status(404).json({}))
 })
 
